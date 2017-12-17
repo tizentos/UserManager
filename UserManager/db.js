6c61b4995866fb6a1b0b6ce3f6be57c5fb713cde
tizentos <tizentos@gmail.com>
2017-12-17 23:55:46 +0100

    Signed-off-by: tizentos <tizentos@gmail.com>

---
+++ UserManager/db.js
@@ -0,0 +1,4 @@
+var mongoose = require('mongoose');
+//mongodb://mongo:27017/blog', function(err, db)
+mongoose.connect('mongodb://mongo:27017/test', function(err, db) { useMongoClient: true });
+//mongoose.connect(process.env.DB_URL, { useMongoClient: true });
