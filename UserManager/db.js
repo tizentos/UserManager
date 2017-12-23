b6eab008663564658e4c1d76e1103f8ce4cc6816
tizentos <tizentos@gmail.com>
2017-12-23 14:54:42 +0100

    Recent changes

--- UserManager/db.js
+++
@@ -1,4 +0,0 @@
-var mongoose = require('mongoose');
-//mongodb://mongo:27017/blog', function(err, db)
-mongoose.connect('mongodb://mongo:27017/test', function(err, db) { useMongoClient: true });
-//mongoose.connect(process.env.DB_URL, { useMongoClient: true });
--- UserManager/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-var app = require('./app');
-var port = process.env.PORT;
-
-var server = app.listen(port, function() {
-  console.log('Express server listening on port ' + port);
-});
